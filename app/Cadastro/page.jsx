509f397b71f92e57c7f1505e7eb272e08b6e5d98
Lucas Rios <lucas.rios@aluno.senai.br>
2024-09-23 11:37:15 -0300

    inputs pagina cadastros

--- app/Cadastro/page.jsx
+++ app/Cadastro/page.jsx
@@ -6,7 +6,43 @@ const cadastro = () => {
         <>
         <div className={styles.cadastro}>
             <div className={styles.card}>
-            <h1>Cadastro</h1>
+
+                <div className={styles.imagem}>
+                    <img src={'/cadastro.svg'}></img>
+                    <div className={styles.placeholder}></div>
+                </div>
+
+                <div className={styles.forms}>
+                    <h1 className={styles.title}> Seja Bem-Vindo</h1>
+                    <p className={styles.text}>Por favor, faça o cadastro antes de continuar</p>
+
+                    <input type="text" placeholder="Nome do Usuário" required className={styles.inputs} />
+                    <input type="email" placeholder="E-mail" required className={styles.inputs} />
+                    <input type="password" placeholder="Senha" required className={styles.inputs} />
+
+                    <p className={styles.curso}>Qual curso você está atualmente fazendo?</p>
+
+                    <div className={styles.opcoes}>
+                    <label className={styles.inputlabel}>
+                    <input type="radio" name="curso" value="Desenvolvimento de sistemas" className={styles.input} />
+                    Desenvolvimento de sistemas
+                    </label>
+
+                    <label className={styles.inputlabel}>
+                    <input type="radio" name="curso" value="Técnico de fabricação mecanica" className={styles.input} />
+                    Técnico de fabricação mecanica
+                    </label>
+
+                    <label className={styles.inputlabel}>
+                    <input type="radio" name="curso" value="Técnico de eletroeletronica" className={styles.input} />
+                    Técnico de eletroeletronica
+                    </label>
+                    </div>
+
+                    <p className={styles.logintext}>Já tem um cadastro?  <a href="/Login" className={styles.login}>Faça o Login</a> </p>
+
+                </div>
+
             </div>
         </div>
         </>
