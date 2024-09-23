ce0a40d0ea7b4badc4fd68ec6ba4db54cf443139
Pablo Couto <pablocouto@aluno.senai.br>
2024-09-23 11:33:02 -0300

    feat: header done

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -1,9 +1,19 @@
 import styles from './styles.module.css'
+import senaiLogo from '../../../public/senai.png'
+import Image from 'next/image'
 
 const Header = () => {
     return (
         <div className={styles.container}>
-            <img src="../../../public/senai.png" alt="logo" className={styles.logo} />
+            <Image src={senaiLogo} alt="logo" className={styles.logo}/>
+            <div className={styles.buttons}>
+                <button className={styles.buttonRegister} >
+                    <p className={styles.buttonTxt}>Cadastrar-se</p>
+                </button>
+                <button className={styles.buttonLogin}>
+                    <p className={styles.buttonTxt}>Login</p>
+                </button>
+            </div>
         </div>
     )
 }
