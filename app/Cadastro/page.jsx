e1ce0c004911281992ab97b34d8d961a264e32ca
Lucas Rios <lucas.rios@aluno.senai.br>
2024-09-23 09:52:22 -0300

    inicio da pagina Cadastro

---
+++ app/Cadastro/page.jsx
@@ -0,0 +1,16 @@
+"use client"
+import styles from "./cadastro.module.css"
+
+const cadastro = () => {
+    return (
+        <>
+        <div className={styles.cadastro}>
+            <div className={styles.card}>
+            <h1>Cadastro</h1>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default cadastro;
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,10 @@
+import Link from 'next/link';
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div>
-      
+      <Link href={'/Cadastro'}>Cadastro</Link>
     </div>
   );
 }
