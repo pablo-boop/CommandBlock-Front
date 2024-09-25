513d42989de24b1555c7452338cb325983c7b0b4
Lucas Rios Ferreira <lucas.rios@aluno.senai.br>
2024-09-25 08:13:53 -0300

    inicio da pagina de Cadastro de Vagas

---
+++ app/CadastroVagas/page.jsx
@@ -0,0 +1,13 @@
+import styles from "./cadastrovagas.module.css";
+import Header from "../components/Header/Header";
+
+const cadastrovagas = () => {
+    return (
+        <>
+          <Header />
+
+        </>
+    )
+}
+
+export default cadastrovagas
