8c7a085daf8926d4412a32d345ed40d14bd29ea4
Lucas Rios Ferreira <lucas.rios@aluno.senai.br>
2024-09-25 09:30:06 -0300

    css cadastro de vagas

--- app/CadastroVagas/page.jsx
+++ app/CadastroVagas/page.jsx
@@ -6,6 +6,83 @@ const cadastrovagas = () => {
         <>
           <Header />
 
+          <div className={styles.container}>
+            <form className={styles.forms}>
+                <h3 className={styles.h3}>Cadastrar Nova Vaga</h3>
+
+                <div className={styles.inputarea}>  
+                    <input type="text"
+                    name="nome da vaga"
+                    placeholder="Nome da Vaga"
+                    className={styles.input}
+                    />
+                </div>
+
+                <div className={styles.inputarea}> 
+                    <input type="text"
+                    name="empresa"
+                    placeholder="Empresa"
+                    className={styles.input}
+                    />
+                </div>
+
+                <div className={styles.form}>
+
+                <div className={styles.inputarea}> 
+                <label className={styles.label}>
+                    <input type="text"
+                    name="cnpj"
+                    placeholder="CNPJ"
+                    className={styles.input2}
+                    />
+                </label>
+                </div>
+
+                <div className={styles.inputarea}> 
+                <label className={styles.label}>
+                    <input type="text"
+                    name="telefone"
+                    placeholder="Telefone"
+                    className={styles.input3}
+                    />
+                </label>
+                </div>
+                </div>
+
+                <div className={styles.inputarea}> 
+                    <input type="email"
+                    name="email"
+                    placeholder="Email"
+                    className={styles.input}
+                    />
+                </div>
+
+                <div className={styles.inputarea}>     
+                    <input type="text"
+                    name="tipoVaga"
+                    placeholder="Tipo da Vaga"
+                    className={styles.input}
+                    />
+                </div>
+
+                <div className={styles.inputarea}>     
+                    <textarea
+                    name="descricao"
+                    placeholder="Descrição da Vaga"
+                    className={styles.textarea}
+                    />
+                </div>    
+
+                <button className={styles.button} type="submit">Cadastrar</button>
+            </form>
+
+            <div className={styles.vagacontainer}>
+                <h3>Vagas</h3>
+                <div className={styles.vagaitem}></div>
+            </div>
+
+          </div>
+
         </>
     )
 }
