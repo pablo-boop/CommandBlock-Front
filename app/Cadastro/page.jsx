2c5c96d869bf511340a5d3950b94819191df754f
Lucas Rios Ferreira <lucas.rios@aluno.senai.br>
2024-10-07 15:05:09 -0300

    consertar css

--- app/Cadastro/page.jsx
+++ app/Cadastro/page.jsx
@@ -17,7 +17,7 @@ const cadastro = () => {
                 </div>
 
                 <div className={styles.forms}>
-                    <h1 className={styles.title}> Seja Bem-Vindo</h1>
+                    <h2 className={styles.title}> Seja Bem-Vindo</h2>
                     <p className={styles.text}>Por favor, faça o cadastro antes de continuar</p>
                     
                     <div className={styles.campos}>
