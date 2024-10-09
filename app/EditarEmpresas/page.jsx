7f17771c5fe3aaa2218539b0a1f178b28251d190
Lucas Rios Ferreira <lucas.rios@aluno.senai.br>
2024-10-09 10:56:42 -0300

    sincronização

--- app/EditarEmpresas/page.jsx
+++ app/EditarEmpresas/page.jsx
@@ -3,21 +3,24 @@ import styles from "./editarempresas.module.css";
 import { FaRegUser } from "react-icons/fa";
 import { MdOutlineEmail } from "react-icons/md";
 import { IoLockClosedOutline } from "react-icons/io5";
-import {FiArrowLeft } from "react-icons/fi";
+import { FiArrowLeft } from "react-icons/fi";
 import { CiPhone } from "react-icons/ci";
 
-import { useState } from "react";
 import { message, Space } from 'antd';
 
-const editarempresas = () => {
+import { useState } from "react";
+import axios from "axios";
 
+const EditarEmpresas = () => {
+
+    
     const [response, setResponse] = useState("");
-
+    
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [cnpj, setCnpj] = useState("");
     const [phone, setPhone] = useState("");
-
+    
     const [messageApi, contextHolder] = message.useMessage();
 
     const clearInputs = () => {
@@ -27,6 +30,7 @@ const editarempresas = () => {
         setPhone("");
     }
 
+    
     const success = (msg) => {
         messageApi.open({
             type: 'success',
@@ -41,14 +45,14 @@ const editarempresas = () => {
         });
     };
 
-
+    
     const handleSubmit = async () => {
         if (name === "" || email === "" || cnpj === "" || phone === "") {
             error("Preencha todos os campos!");
         } else {
             try {
                 const response = await fetch(`https://49ab-201-63-78-210.ngrok-free.app/users`, {
-                    method: 'POST',
+                    method: 'PUT', 
                     headers: new Headers({
                         'Content-Type': 'application/json',
                         "ngrok-skip-browser-warning": "69420",
@@ -58,109 +62,107 @@ const editarempresas = () => {
                         email: email,
                         cnpj: cnpj,
                         phone: phone,
-                        type: "Aluno"
                     })
                 });
-    
+
                 if (!response.ok) {
                     const errorText = await response.text();
                     let errorMessage = response.statusText;
-    
+
                     try {
                         const errorJson = JSON.parse(errorText);
                         errorMessage = errorJson.message;
                     } catch (e) {
                         console.error("Erro ao parsear JSON:", e);
                     }
-    
+
                     throw new Error(errorMessage);
                 }
-    
+
                 const responseData = await response.json();
-                clearInputs()
+                clearInputs();
                 success(responseData.message);
-    
-                // Redireciona para a página de Login
-                setTimeout(() => {
-                    window.location.href = '/Login';
-                }, 2000);
-    
+
             } catch (err) {
                 console.error(err);
-                error(err.message); // Exibe apenas a mensagem de erro
+                error(err.message); 
             }
         }
     };
 
-
-
     return (
         <>
-        <div className={styles.cadastro}>
+            <div className={styles.cadastro}>
             {contextHolder}
-            <div className={styles.card}>
+                <div className={styles.card}>
+                    <div className={styles.imagem}>
+                        <FiArrowLeft className={styles.arrow} />
+                        <img src={'/cadastro.svg'} alt="Editar Empresas" />
+                    </div>
 
-                <div className={styles.imagem}>
-                <FiArrowLeft className={styles.arrow} />
-                    <img src={'/cadastro.svg'}></img>
+                    <div className={styles.forms}>
+                        <h1 className={styles.title}> Editar Empresas</h1>
+                        <p className={styles.text}>Por favor, Preencha o campo de nome e atualize os outros dados</p>
+
+                        <div className={styles.campos}>
+                            <FaRegUser className={styles.icone} />
+                            <input
+                                value={name}
+                                onChange={e => setName(e.target.value)}
+                                type="text"
+                                placeholder="Nome da Empresa"
+                                required
+                                className={styles.inputs}
+                            />
+                        </div>
+
+                        <div className={styles.campos}>
+                            <MdOutlineEmail className={styles.icone} />
+                            <input
+                                value={email}
+                                onChange={e => setEmail(e.target.value)}
+                                type="email"
+                                placeholder="E-mail"
+                                required
+                                className={styles.inputs}
+                            />
+                        </div>
+
+                        <div className={styles.campos}>
+                            <IoLockClosedOutline className={styles.icone} />
+                            <input
+                                value={cnpj}
+                                onChange={e => setCnpj(e.target.value)}
+                                type="text"
+                                placeholder="CNPJ"
+                                required
+                                className={styles.inputs}
+                            />
+                        </div>
+
+                        <div className={styles.campos}>
+                            <CiPhone className={styles.icone} />
+                            <input
+                                value={phone}
+                                onChange={e => setPhone(e.target.value)}
+                                type="text"
+                                placeholder="Telefone"
+                                required
+                                className={styles.inputs}
+                            />
+                        </div>
+
+                        <Space>
+                            <button className={styles.button} onClick={handleSubmit}>
+                                Atualizar
+                            </button>
+                        </Space>
+
+                    </div>
                 </div>
-
-                <div className={styles.forms}>
-                    <h1 className={styles.title}> Editar Empresas</h1>
-                    <p className={styles.text}>Por favor, Preencha o campo de nome e atualize os outros dados</p>
-                    
-                    <div className={styles.campos}>
-                    <FaRegUser className={styles.icone} />
-                    <input 
-                    value={name}
-                    onChange={e => setName(e.target.value)}
-                    type="text" 
-                    placeholder="Nome da Empesa" 
-                    required 
-                    className={styles.inputs} />
-                    </div>
-
-                    <div className={styles.campos}>
-                    <MdOutlineEmail className={styles.icone} />
-                    <input 
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                    type="email" 
-                    placeholder="E-mail" 
-                    required 
-                    className={styles.inputs} />
-                    </div>
-
-                    <div className={styles.campos}>
-                    <IoLockClosedOutline className={styles.icone} />
-                    <input 
-                    value={cnpj}
-                    onChange={e => setCnpj(e.target.value)}
-                    type="password" 
-                    placeholder="CNPJ" 
-                    required 
-                    className={styles.inputs} />
-                    </div>
-
-                    <div className={styles.campos}>
-                    <CiPhone className={styles.icone} />
-                    <input 
-                    value={phone}
-                    onChange={e => setPhone(e.target.value)}
-                    type="number" 
-                    placeholder="Telefone" 
-                    required 
-                    className={styles.inputs} />
-                    </div>
-
-                    <button className={styles.button} onClick={handleSubmit}>Atualizar</button>
-
-                </div>
-
             </div>
-        </div>
         </>
-    )
+    );
 }
 
-export default editarempresas;
+export default EditarEmpresas;
