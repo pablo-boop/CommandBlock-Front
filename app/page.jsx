979fcaa46099d18049c3989ac40c3d0b320b4e8a
Aline <123502100+Alineevang@users.noreply.github.com>
2024-10-09 11:36:02 -0300

    feat: desenvolvimento da tela Home

--- app/page.jsx
+++ app/page.jsx
@@ -4,8 +4,17 @@ import Header from "./components/Header/Header";
 export default function Home() {
   return (
     <div>
-      <Header />
+      <Header/>
+      <div className={styles.container}>
+      <h2 className={styles.titulo}>Tipos de Vagas</h2>
+      <img src="./equipe.jpg" className={styles.image} />
+      <h2 className={styles.subtitulo}>Jovem Aprendiz</h2>
+      <p className={styles.texto}>O SENAI está oferecendo oportunidades de trabalho para aqueles que buscam estabilidade e desejam integrar uma instituição respeitada, 
+      dedicada à formação de profissionais e ao desenvolvimento da indústria brasileira. Estamos contratando para diversas áreas com vagas 
+      efetivas, proporcionando não apenas um emprego, mas também uma chance de crescimento em um ambiente que valoriza a inovação, a 
+      educação de qualidade e o desenvolvimento contínuo.</p>
 
+      </div>
     </div>
   );
 } 
